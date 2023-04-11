a0f943bfab9f9acebf76170155ccad50c1e625fd
Luke Griffith <lukegriffith@gmail.com>
2023-04-11 15:56:57 +0000

    add files

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export interface CompatibilityItem {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export interface FAQItem {
+  question: string;
+  answer: string;
+}
+
+export interface FooterLink {
+  description: string;
+  icon: string;
+  url: string;
+}
+
+export interface NavItem {
+  title: string;
+  url: string;
+}
+
+export interface ShowcaseSite {
+  title: string;
+  image: ImageMetadata;
+  url: string;
+}
