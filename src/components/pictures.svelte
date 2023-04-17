50b0db625deed1e09f7de42f20edc3f3b6f8e1db
just-luk <lukegriffith@gmail.com>
2023-04-16 21:25:08 -0400

    add reduced motion

--- src/components/pictures.svelte
+++ src/components/pictures.svelte
@@ -2,10 +2,6 @@
     import { fade } from "svelte/transition";
     import { quintOut } from "svelte/easing";
     let index = 0;
-    const increaseIndex = () => {
-        index = (index + 1) % 6;
-    };
-    setInterval(increaseIndex, 5000);
     const images = [
         "images/hsf1.jpg",
         "images/hsf2.jpg",
@@ -14,6 +10,12 @@
         "images/hsf5.jpg",
         "images/hsf6.jpg",
     ];
+    const increaseIndex = () => {
+        index = (index + 1) % 6;
+    };
+    const isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+    if (!isReduced)
+        setInterval(increaseIndex, 5000);
 </script>
 
 {#each [images[index]] as item (index)}
