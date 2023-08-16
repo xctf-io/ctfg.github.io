8fac2ef96e99be949437b3d11f7a8cf58624d127
just-luk <lukegriffith@gmail.com>
2023-08-15 20:32:18 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,21 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
 
+
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mcpshsf.com/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    svelte(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), svelte(), sitemap()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
